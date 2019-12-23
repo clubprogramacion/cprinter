a0269ccbe07d73b482583c8750b9cf64baa1d60f
Disble <decoder@hotmail.es>
2019-12-23 11:46:24 -0500

    feat: add express and service index.html

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const printer = require('pdf-to-printer');
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
-async function cprint(documentToPrint) {
-    try {
-        let resp = await printer.print(documentToPrint);
-        console.log(resp);
-    } catch (error) {
-        console.error(error);
-    }
-}
+// settings
+app.set('appName', 'cprinter');
+app.set('port', process.env.PORT || 3000);
 
-let documentToPrint = 'cpriner.pdf';
-cprint(documentToPrint);
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.all('/print', (req, res, next) => {
+    console.log('Nueva impresión');
+    next();
+});
+
+// routes
+app.post('/print', (req, res) => {
+    console.log(req.body);
+    res.send('FILE PRINTING...');
+});
+
+app.use(express.static('public')); // como no encuentra la ruta / retorna el index.html
+
+app.listen(app.get('port'), () => {
+    console.log(`App name: ${app.get('appName')}`);
+    console.log(`Server on port ${app.get('port')}`);
+});
