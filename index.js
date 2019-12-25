6a55289d8ecd09fc2d86402ff20efcc560f40162
NeiderRequene <requeneneider@gmail.com>
2019-12-25 02:40:47 -0500

    feat: Get and save file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const fileUpload = require('express-fileupload');
 const app = express();
 
 // settings
@@ -11,6 +12,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.use(fileUpload());
+
 app.all('/print', (req, res, next) => {
     console.log('Nueva impresión');
     next();
@@ -18,8 +21,20 @@ app.all('/print', (req, res, next) => {
 
 // routes
 app.post('/print', (req, res) => {
-    console.log(req.body);
-    res.send('FILE PRINTING...');
+    console.log('Aqui',req.body);
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+    let file_print = req.files.file;
+    file_print.mv(`./files/${file_print.name}`,err => {
+        if (err) {
+            console.log('Error',err);
+            
+            return res.status(500).send({ message: err });  
+        } 
+        return res.status(200).send({ message : 'File upload' })
+    })
+    // res.send('FILE PRINTING...');
+    // next();
 });
 
 app.use(express.static('public')); // como no encuentra la ruta / retorna el index.html
